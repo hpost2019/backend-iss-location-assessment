b3c667ef73caaa633aff1290ce761ad8f3735da2
Howard Post <hpost2012@gmail.com>
2020-06-17 04:08:20 -0700

    finished part D.  Rewrote code for organization and added features

--- iss.py
+++ iss.py
@@ -4,6 +4,10 @@ __author__ = 'hpost2019'
 import requests
 import json
 import turtle
+import time
+
+iss = turtle.Turtle()
+in_loc = turtle.Turtle()
 
 
 def list_astronauts():
@@ -18,38 +22,78 @@ in space. They are:'.format(res_obj['number']))
 '.format(person['name'], person['craft']))
 
 
-def retrieve_location():
+def iss_location():
     """Queries API to get current longitude and latitude
     of ISS.  Returns object with data"""
     res = requests.get('http://api.open-notify.org/iss-now.json')
-    res_obj = json.loads(res.text)
-    return {'long': res_obj['iss_position']['longitude'],
-            'lat': res_obj['iss_position']['latitude'],
-            'timestamp': res_obj['timestamp']}
+    if res.status_code == 200:
+        res_obj = json.loads(res.text)
+        lat = float(res_obj['iss_position']["latitude"])
+        long = float(res_obj['iss_position']["longitude"])
+        move_iss(lat, long)
+    else:
+        print("Houston we need some help here!! ", res.status_code)
+    reload_iss = turtle.getcanvas()
+    reload_iss.after(5000, iss_location)
 
 
-def plot_iss():
-    """Takes in current location of ISS and plots
-    in on a word map"""
-    image = 'iss.gif'
-    screen = turtle.Screen()
+def screen_setup(screen):
+    """Does the required setup of the screen.  Set initial size,
+    sets the background image, sets world cordinates so
+    lat and long work, and finally reg shapes."""
+    global iss, in_loc
     screen.setup(720, 360)
+    screen.title("Where in the world is ISS?")
     screen.bgpic('map.gif')
     screen.setworldcoordinates(-180, -90, 180, 90)
-    iss = turtle.Turtle()
-    turtle.register_shape(image)
-    iss.shape(image)
-    loc_obj = retrieve_location()
+    in_loc.shape('circle')
+    in_loc.turtlesize(.3, .3, .3)
+    in_loc.color('yellow')
+    turtle.register_shape("iss.gif")
+    iss.shape("iss.gif")
+
+
+def move_iss(lat, long):
+    """Takes in current location of ISS and plots
+    in on a word map"""
+    global iss
     iss.penup()
-    iss.goto(float(loc_obj['long']), float(loc_obj['lat']))
-    screen.exitonclick()
+    iss.goto(long, lat)
+
+
+def next_over_indianapolis(long, lat):
+    payload = {'lat': lat, 'lon': long}
+    res = requests.get('http://api.open-notify.org/iss-pass.json',
+                       params=payload)
+    if res.status_code == 200:
+        res_obj = json.loads(res.text)
+        return res_obj['response'][0]['risetime']
+    else:
+        print("Houston we have another problem!! ", res.status_code)
+
+
+def plot_indianapolis():
+    """Plots the location on the map of Indianapolis, Indiana.
+    Then writes when ISS will next be over that location. """
+    global in_loc
+
+    in_loc.penup()
+    in_loc.goto(-86.159536, 39.778117)
+    next_time = next_over_indianapolis(-86.159536, 39.778117)
+    next_time = time.ctime(next_time)
+    in_loc.write(next_time)
 
 
 def main():
+    global iss, in_loc
+    screen = turtle.Screen()
     list_astronauts()
-    plot_iss()
+    screen_setup(screen)
+    plot_indianapolis()
+    iss_location()
     pass
 
 
 if __name__ == '__main__':
     main()
+    turtle.mainloop()
