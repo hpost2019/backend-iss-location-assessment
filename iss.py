c07c706cd00bed88e163da3065dc21dd3fa4068c
Howard Post <hpost2012@gmail.com>
2020-06-17 00:48:07 -0700

    completed part A

--- iss.py
+++ iss.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'hpost2019'
+import requests
+import json
+
+
+def list_astronauts():
+    """Queries API to get list of astronauts in space
+    and what spacecraft they are currently on"""
+    res = requests.get('http://api.open-notify.org/astros.json')
+    res_obj = json.loads(res.text)
+    print('There are currently {0} astronauts \
+in space. They are:'.format(res_obj['number']))
+    for person in res_obj['people']:
+        print('\t{0} who is on spacecraft: {1}\
+'.format(person['name'], person['craft']))
 
 
 def main():
+    list_astronauts()
     pass
 
 
